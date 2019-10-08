9fc5a36e593b058376fab3a4cecd1afb57e1e515
Mac Ignacio <mack_ignacio@yahoo.com>
2019-10-09 02:50:25 +0800

    Add interface.ts

---
+++ src/lib/Interface.ts
@@ -0,0 +1,16 @@
+export interface IFunctions<Chain> {
+  body(): this;
+  params(): this;
+  isBoolean(value?: RegExp): Chain;
+  isString(value?: RegExp): Chain;
+  isAddress(value?: RegExp): Chain;
+  minLength(value: number): Chain;
+  maxLength(value: number): Chain;
+  isDate(): Chain;
+  isEmail(): Chain;
+  isPassword(): Chain;
+}
+
+export interface IChain extends IFunctions<IChain> {
+  (req: any, res: any, next: (error?: any) => void): void;
+}
