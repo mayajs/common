0a95500425ac43575e239ba2f021e4dfc8391358
Mac Ignacio <mack_ignacio@yahoo.com>
2019-10-09 15:42:01 +0800

    Add validate and isRegExp method

--- src/lib/Functions.ts
+++ src/lib/Functions.ts
@@ -14,63 +14,81 @@ export class Functions<Chain> implements IFunctions<Chain> {
     return this;
   }
 
+  validate(field: any, callback: boolean): boolean {
+    return typeof field !== "undefined" || field !== null ? callback : true;
+  }
+
   isNumber(): Chain {
-    const test = (field: any): boolean => typeof field === "number" && !isNaN(field);
+    const condition = (field: any): boolean => typeof field === "number" && !isNaN(field);
+    const test = (field: any): boolean => this.validate(field, condition(field));
     this.runner.addValidation(test, "is not a number");
     return this.middleware;
   }
 
   isBoolean(): Chain {
-    const test = (field: any): boolean => typeof field === "boolean";
+    const condition = (field: any): boolean => typeof field === "boolean";
+    const test = (field: any): boolean => this.validate(field, condition(field));
     this.runner.addValidation(test, "is not a boolean");
     return this.middleware;
   }
 
-  isString(regex?: RegExp): Chain {
-    const validChars = regex ? regex : /^[A-Za-z0-9_\-@.,()\s]*$/;
-    const test = (field: any): boolean => typeof field === "string" && field.length !== 0 && new RegExp(validChars).test(field);
+  isRegExp(regex: RegExp): Chain {
+    const condition = (field: any) => typeof field === "string" && new RegExp(regex).test(field);
+    const test = (field: any): boolean => this.validate(field, condition(field));
     this.runner.addValidation(test, "is not a string or not a valid string format");
     return this.middleware;
   }
 
-  isAddress(regex?: RegExp): Chain {
-    const validChars = regex ? regex : /^[a-zA-Z0-9-#_\-.,()@\s]*$/;
-    const test = (field: any): boolean => typeof field === "string" && field.length !== 0 && new RegExp(validChars).test(field);
+  isString(): Chain {
+    const validChars = /^[A-Za-z0-9.,\s]*$/;
+    const condition = (field: any) => typeof field === "string" && new RegExp(validChars).test(field);
+    const test = (field: any): boolean => this.validate(field, condition(field));
+    this.runner.addValidation(test, "is not a string or not a valid string format");
+    return this.middleware;
+  }
+
+  isAddress(): Chain {
+    const validChars = /^[a-zA-Z0-9#_\-.,()@\s]*$/;
+    const condition = (field: any) => typeof field === "string" && new RegExp(validChars).test(field);
+    const test = (field: any): boolean => this.validate(field, condition(field));
     this.runner.addValidation(test, "is not a valid address format");
     return this.middleware;
   }
 
   minLength(value: number): Chain {
-    const test = (field: any): boolean => field.length >= value;
+    const test = (field: any): boolean => this.validate(field, field.length >= value);
     this.runner.addValidation(test, `must have a length of ${value}`);
     return this.middleware;
   }
 
   maxLength(value: number): Chain {
-    const test = (field: any): boolean => field.length <= value;
+    const test = (field: any): boolean => this.validate(field, field.length <= value);
     this.runner.addValidation(test, `must be ${value} in length or fewer`);
     return this.middleware;
   }
 
   isDate(): Chain {
-    const test = (field: any): boolean => {
+    const condition = (field: any): boolean => {
       const date = new Date(field);
       return Object.prototype.toString.call(date) === "[object Date]" && !isNaN(date.getTime());
     };
+    const test = (field: any): boolean => this.validate(field, condition(field));
     this.runner.addValidation(test, `must be valid date format`);
     return this.middleware;
   }
 
   isEmail(): Chain {
     const validChars = /^[a-zA-Z0-9_.-]+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
-    const test = (field: any): boolean => typeof field === "string" && field.length !== 0 && new RegExp(validChars).test(field);
+    const condition = (field: any) => typeof field === "string" && new RegExp(validChars).test(field);
+    const test = (field: any): boolean => this.validate(field, condition(field));
     this.runner.addValidation(test, "is not a valid email");
     return this.middleware;
   }
 
   isPassword(): Chain {
     const validChars = /((?=.*\d)(?=.*[A-Z])(?=.*\W))/;
-    const test = (field: any): boolean => typeof field === "string" && field.length !== 0 && new RegExp(validChars).test(field);
+    const condition = (field: any) => typeof field === "string" && new RegExp(validChars).test(field);
+    const test = (field: any): boolean => this.validate(field, condition(field));
     this.runner.addValidation(test, "is not a valid password");
     return this.middleware;
   }
