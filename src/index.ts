df6362e691f4adf1798457f31b71bae3605ca897
Mac Ignacio <mack_ignacio@yahoo.com>
2019-10-09 13:07:43 +0800

    Add method factory

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
+import "reflect-metadata";
 import { NextFunction, Request, Response } from "express";
 
+import { Runner } from "./lib/Runner";
 import { IChain } from "./lib/Interface";
 import { Functions } from "./lib/Functions";
-import { Runner } from "./lib/Runner";
 import { Container } from "./lib/Container";
+import { MethodDecoratorFactory } from "./lib/MethodFactory";
 
 export function Check(fieldName: string): IChain {
   const runner = new Runner(fieldName);
@@ -16,4 +18,44 @@ export function Check(fieldName: string): IChain {
     }
   };
   return Object.assign(middleware, Container(new Functions<IChain>(runner, middleware)));
+}
+
+/**
+ * GET Method Decorator
+ * @param properties - Sets the path and validation for this method.
+ */
+export function Get(properties: { path: string; middlewares?: Array<(...args: any[]) => void> }): MethodDecorator {
+  return MethodDecoratorFactory("get")(properties);
+}
+
+/**
+ * POST Method Decorator
+ * @param properties - Sets the path and validation for this method.
+ */
+export function Post(properties: { path: string; middlewares?: Array<(...args: any[]) => void> }): MethodDecorator {
+  return MethodDecoratorFactory("post")(properties);
+}
+
+/**
+ * PATCH Method Decorator
+ * @param properties - Sets the path and validation for this method.
+ */
+export function Patch(properties: { path: string; middlewares?: Array<(...args: any[]) => void> }): MethodDecorator {
+  return MethodDecoratorFactory("patch")(properties);
+}
+
+/**
+ * PUT Method Decorator
+ * @param properties - Sets the path and validation for this method.
+ */
+export function Put(properties: { path: string; middlewares?: Array<(...args: any[]) => void> }): MethodDecorator {
+  return MethodDecoratorFactory("put")(properties);
+}
+
+/**
+ * DELETE Method Decorator
+ * @param properties - Sets the path and validation for this method.
+ */
+export function Delete(properties: { path: string; middlewares?: Array<(...args: any[]) => void> }): MethodDecorator {
+  return MethodDecoratorFactory("delete")(properties);
 }
