498ab8ac1e60cb09870a5e3b16b3e2db7a9412e9
Mack Ignacio <mack_ignacio@yahoo.com>
2020-11-30 08:23:41 +0800

    FEAT: Exports methods from lib

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,10 @@
 // EXTERNAL IMPORTS
 import "reflect-metadata";
 import { NextFunction, Request, Response } from "@mayajs/core";
-import { Callback } from "@mayajs/core/types";
 
 // LOCAL IMPORTS
 import { Runner, Container, Functions } from "./utils";
-import { MethodFactoryOptions } from "./types";
-import { MethodDecoratorFactory } from "./lib";
+
 import { IChain } from "./interfaces";
 
 /**
@@ -26,52 +24,4 @@ export function Check(fieldName: string): IChain {
   return Object.assign(middleware, Container(new Functions<IChain>(runner, middleware)));
 }
 
-/**
- * GET Method Decorator
- * @param properties - Sets the properties of the method.
- * @param properties.path - Sets the path of the route.
- * @param properties.middlewares - Sets an array of middlewares for this method.
- */
-export function Get(properties?: MethodFactoryOptions, middlewares: Callback[] = []): MethodDecorator {
-  return MethodDecoratorFactory("get")(properties, middlewares);
-}
-
-/**
- * POST Method Decorator
- * @param properties - Sets the properties of the method.
- * @param properties.path - Sets the path of the route.
- * @param properties.middlewares - Sets an array of middlewares for this method.
- */
-export function Post(properties?: MethodFactoryOptions, middlewares: Callback[] = []): MethodDecorator {
-  return MethodDecoratorFactory("post")(properties, middlewares);
-}
-
-/**
- * PATCH Method Decorator
- * @param properties - Sets the properties of the method.
- * @param properties.path - Sets the path of the route.
- * @param properties.middlewares - Sets an array of middlewares for this method.
- */
-export function Patch(properties?: MethodFactoryOptions, middlewares: Callback[] = []): MethodDecorator {
-  return MethodDecoratorFactory("patch")(properties, middlewares);
-}
-
-/**
- * PUT Method Decorator
- * @param properties - Sets the properties of the method.
- * @param properties.path - Sets the path of the route.
- * @param properties.middlewares - Sets an array of middlewares for this method.
- */
-export function Put(properties?: MethodFactoryOptions, middlewares: Callback[] = []): MethodDecorator {
-  return MethodDecoratorFactory("put")(properties, middlewares);
-}
-
-/**
- * DELETE Method Decorator
- * @param properties - Sets the properties of the method.
- * @param properties.path - Sets the path of the route.
- * @param properties.middlewares - Sets an array of middlewares for this method.
- */
-export function Delete(properties?: MethodFactoryOptions, middlewares: Callback[] = []): MethodDecorator {
-  return MethodDecoratorFactory("delete")(properties);
-}
+export * from "./lib/methods";
