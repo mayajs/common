ae4fe0dbef122dec2b762764e60eaaf9c8c97df2
Mac <mack_ignacio@yahoo.com>
2022-03-28 20:29:54 +0800

    Fix isOptional implementation

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -19,7 +19,9 @@ export class Functions<Chain> implements IFunctions<Chain> {
   }
 
   required(message?: string): Chain {
-    const test = (field: any): boolean => this.utils.notUndefined(field);
+    const test = (field: any): boolean => {
+      return this.utils.notUndefined(field) || this.utils.isObject(field) || Array.isArray(field);
+    };
     this.runner.addValidation(test, "is required", { message, isOptional: false });
     return this.middleware;
   }
@@ -27,49 +29,49 @@ export class Functions<Chain> implements IFunctions<Chain> {
   isNumber(message?: string): Chain {
     const condition = (field: any): boolean => this.utils.number(field) && !isNaN(field);
     const test = (field: any): boolean => this.utils.validate(field, () => condition(field));
-    this.runner.addValidation(test, "is not a number", { message, isOptional: false });
+    this.runner.addValidation(test, "is not a number", { message, isOptional: true });
     return this.middleware;
   }
 
   isBoolean(message?: string): Chain {
     const condition = (field: any): boolean => this.utils.boolean(field);
     const test = (field: any): boolean => this.utils.validate(field, () => condition(field));
-    this.runner.addValidation(test, "is not a boolean", { message, isOptional: false });
+    this.runner.addValidation(test, "is not a boolean", { message, isOptional: true });
     return this.middleware;
   }
 
   isRegExp(regex: RegExp, message?: string): Chain {
     const condition = (field: any) => this.utils.string(field) && this.utils.regExpTest(field, regex);
     const test = (field: any): boolean => this.utils.validate(field, () => condition(field));
-    this.runner.addValidation(test, `not match the regexp pattern of ${regex}`, { message, isOptional: false });
+    this.runner.addValidation(test, `not match the regexp pattern of ${regex}`, { message, isOptional: true });
     return this.middleware;
   }
 
   isString(message?: string): Chain {
     const condition = (field: any) => this.utils.string(field) && this.utils.regExpTest(field, /^[A-Za-z0-9.,\s]*$/);
     const test = (field: any): boolean => this.utils.validate(field, () => condition(field));
-    this.runner.addValidation(test, "is not a string or not a valid string format", { message, isOptional: false });
+    this.runner.addValidation(test, "is not a string or not a valid string format", { message, isOptional: true });
     return this.middleware;
   }
 
   isAddress(message?: string): Chain {
     const condition = (field: any) => this.utils.string(field) && this.utils.regExpTest(field, /^[a-zA-Z0-9#_\-.,()@\s]*$/);
     const test = (field: any): boolean => this.utils.validate(field, () => condition(field));
-    this.runner.addValidation(test, "is not a valid address format", { message, isOptional: false });
+    this.runner.addValidation(test, "is not a valid address format", { message, isOptional: true });
     return this.middleware;
   }
 
   minLength(value: number, message?: string): Chain {
     const condition = (field: any): boolean => (this.utils.number(field) ? field >= value : field.length >= value);
     const test = (field: any): boolean => this.utils.validate(field, () => condition(field));
-    this.runner.addValidation(test, `must have a length of ${value}`, { message, isOptional: false });
+    this.runner.addValidation(test, `must have a length of ${value}`, { message, isOptional: true });
     return this.middleware;
   }
 
   maxLength(value: number, message?: string): Chain {
     const condition = (field: any): boolean => (this.utils.number(field) ? field <= value : field.length <= value);
     const test = (field: any): boolean => this.utils.validate(field, () => condition(field));
-    this.runner.addValidation(test, `must not exceed a length of ${value}`, { message, isOptional: false });
+    this.runner.addValidation(test, `must not exceed a length of ${value}`, { message, isOptional: true });
     return this.middleware;
   }
 
@@ -79,7 +81,7 @@ export class Functions<Chain> implements IFunctions<Chain> {
       return Object.prototype.toString.call(date) === "[object Date]" && !isNaN(date.getTime());
     };
     const test = (field: any): boolean => this.utils.validate(field, () => condition(field));
-    this.runner.addValidation(test, `must be valid date format`, { message, isOptional: false });
+    this.runner.addValidation(test, `must be valid date format`, { message, isOptional: true });
     return this.middleware;
   }
 
@@ -87,7 +89,7 @@ export class Functions<Chain> implements IFunctions<Chain> {
     const validChars = /^[a-zA-Z0-9_.-]+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
     const condition = (field: any) => this.utils.string(field) && this.utils.regExpTest(field, validChars);
     const test = (field: any): boolean => this.utils.validate(field, () => condition(field));
-    this.runner.addValidation(test, "is not a valid email", { message, isOptional: false });
+    this.runner.addValidation(test, "is not a valid email", { message, isOptional: true });
     return this.middleware;
   }
 
@@ -95,7 +97,7 @@ export class Functions<Chain> implements IFunctions<Chain> {
     const validChars = /((?=.*\d)(?=.*[A-Z])(?=.*\W))/;
     const condition = (field: any) => this.utils.string(field) && this.utils.regExpTest(field, validChars);
     const test = (field: any): boolean => this.utils.validate(field, () => condition(field));
-    this.runner.addValidation(test, "is not a valid password", { message, isOptional: false });
+    this.runner.addValidation(test, "is not a valid password", { message, isOptional: true });
     return this.middleware;
   }
 
@@ -107,7 +109,7 @@ export class Functions<Chain> implements IFunctions<Chain> {
 
   isArray(message?: string): Chain {
     const test = (field: any): boolean => this.utils.validate(field, () => Array.isArray(field));
-    this.runner.addValidation(test, "is not an array", { message, isOptional: false });
+    this.runner.addValidation(test, "is not an array", { message, isOptional: true });
     return this.middleware;
   }
 
@@ -121,7 +123,7 @@ export class Functions<Chain> implements IFunctions<Chain> {
       return array.includes(field);
     };
     const test = (field: any): boolean => this.utils.validate(field, () => condition(field));
-    this.runner.addValidation(test, `only accepts the following value(s) [ ${array.join(", ")} ]`, { message, isOptional: false });
+    this.runner.addValidation(test, `only accepts the following value(s) [ ${array.join(", ")} ]`, { message, isOptional: true });
     return this.middleware;
   }
 }
